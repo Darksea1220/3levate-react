81a95920b3a6b4666fb4857f827f4bbe44623abf
Maria Jose Barney <maria.barney1@correo.icesi.edu.co>
2023-11-01 18:56:28 -0500

    Tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import { Header, Footer, Home } from './Components/index'
 
 function App () {
   return (
+
     <>
+      <div className='flex items-center justify-center flex-col text-center pt-20 pb-6' />
+      <h1 className='text-4xl md:text-7x1 mb-1 md:mb-3'>
+        Prueba TW
+      </h1>
+      <div />
       <Header />
       <Home />
       <Footer />

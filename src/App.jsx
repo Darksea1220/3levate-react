61b0767cef6eaf15a66204daf8b88d0ba98f61cc
Maria Jose Barney <maria.barney1@correo.icesi.edu.co>
2023-11-01 19:14:35 -0500

    Tailwind working

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import { Header, Footer, Home } from './Components/index'
 
 function App () {
   return (
-
     <>
-      <div className='flex items-center justify-center flex-col text-center pt-20 pb-6' />
-      <h1 className='text-4xl md:text-7x1 mb-1 md:mb-3'>
-        Prueba TW
-      </h1>
-      <div />
+      <div className='flex items-center justify-center text-center pt-20 pb-6'>
+        <h1 className='text-4xl md:text-7x1 mb-1 md:mb-3'>
+          Prueba TW
+        </h1>
+      </div>
       <Header />
       <Home />
       <Footer />

c50ee6bff3c9f3e4271d60c1b71297ce8330361a
Maria Jose Barney <maria.barney1@correo.icesi.edu.co>
2023-11-24 19:30:58 -0500

    Feat: router arreglado

---
+++ src/AppRouter.jsx
@@ -0,0 +1,46 @@
+import { Routes, Route } from 'react-router-dom'
+import { PageNotFound, ProjectsPage } from './pages'
+// import { ServicesRouter } from './routers/ServicesRouter'
+import { ProjectDetail } from './pages/Pdetail/ProjectDetail'
+import { Footer, Header, Home, Login } from './Components'
+import Layout from './Components/Layout/Layout'
+// aqui estuvo dj
+export function AppRouter () {
+  return (
+    <main>
+      <Routes>
+        <Route
+          path=''
+          element={<Login />}
+        />
+        <Route
+          path='/'
+          element={
+            <>
+              <Header />
+              <Layout />
+              <Footer />
+            </>
+        }
+        >
+          <Route
+            path='/home'
+            element={<Home />}
+          />
+          <Route
+            path='/projects'
+            element={<ProjectsPage />}
+          />
+          <Route
+            path='/projects/project/:title'
+            element={<ProjectDetail />}
+          />
+          <Route
+            path='/*'
+            element={<PageNotFound />}
+          />
+        </Route>
+      </Routes>
+    </main>
+  )
+}

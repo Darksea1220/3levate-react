d6e02a879158cc143db38a7c13e86a9eedfa34d2
juangus2345 <juangushd2345@gmail.com>
2023-11-27 21:56:38 -0500

    Funciona la pagina services

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './services.css'
+
+export function Services () {
+  return (
+    <div className='services'>
+      <h1>Services</h1>
+      <p>Design System Development</p>
+      <p>User Interface (UI) Design</p>
+      <p>Digital Product Development</p>
+      <p>Product development and enhancement</p>
+      <p>Prototyping and Testing</p>
+      <p>User Experience (UX) Design</p>
+      <p>User-Centered Research</p>
+      <p>Design Thinking and Ideation</p>
+      <p>Brand Communication and management</p>
+      <p>Content Strategy and Copywriting</p>
+      <p>Project Management and Collaboration</p>
+      <p>Quality Assurance (QA) and Testing</p>
+    </div>
+  )
+}
